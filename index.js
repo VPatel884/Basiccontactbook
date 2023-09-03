2c1ab83367612fb66c8fbffbde0e6e948f8bfb22
VPatel884 <vpatel884@gmail.com>
2023-09-03 10:18:34 +0000

    Created function to add contact.

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function mainMenu() {
   console.log('2. View Contact');
   console.log('3. Exit')
 }
+
+function addContact() {
+  console.log('\nAdd A New Contact');
+  let name = readlineSync.question('Enter Name: ');
+  let mobileNo = readlineSync.question('Enter Mobile Number: ');
+  let email = readlineSync.question('Enter Email: ');
+
+  contact.push({name,mobileNo,email});
+  console.log('Contact Added Successfully.')
+}
+
