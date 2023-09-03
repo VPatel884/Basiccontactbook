9a88b6a965b719a72b4958e4ac18ae50af74e2a4
VPatel884 <vpatel884@gmail.com>
2023-09-03 10:09:44 +0000

    Created contact's main menu.

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const readlineSync = require('readline-sync');
 
 let contact = [];
 
+function mainMenu() {
+  console.log('\nContact Book Menu: ');
+  console.log('1. Add Contact');
+  console.log('2. View Contact');
+  console.log('3. Exit')
+}
